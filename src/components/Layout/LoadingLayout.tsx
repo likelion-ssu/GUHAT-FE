fd4acb25117803e8f6ef86a8352aa5d794e4e166
Hyomk <qet557@gmail.com>
2022-12-05 16:19:45 +0900

    feat :  loading recoil #11

--- src/components/Layout/LoadingLayout.tsx
+++ src/components/Layout/LoadingLayout.tsx
@@ -1,4 +1,6 @@
+import { errorState, loadingState } from "@/storage/recoil/loadingState";
 import { useEffect, useState } from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
 import { LoadingContainer } from "./LoadingLayout.style";
 const LoadingLayout = ({
     msg,
@@ -9,6 +11,9 @@ const LoadingLayout = ({
     error?: string;
     success?: boolean;
 }) => {
+    const [loading, setLoading] = useRecoilState(loadingState);
+    const errorMsg = useRecoilValue(errorState);
+
     const [message, setMessage] = useState<string | null>(
         msg ? msg : "Loading..."
     );
@@ -16,15 +21,17 @@ const LoadingLayout = ({
         setTimeout(() => {
             setMessage("서버 요청이 많아 대기 시간이 길어지고 있습니다...");
         }, 5000);
-        setTimeout(() => {
-            setMessage(null);
-        }, 6000);
     }, []);
+
+    useEffect(() => {
+        console.log(errorMsg);
+    }, [errorMsg]);
+
     return (
         <>
-            {message || success ? (
+            {loading || errorMsg ? (
                 <LoadingContainer>
-                    {error ? <h1>{error}</h1> : <h1>{message}</h1>}
+                    {errorMsg ? <h1>{errorMsg}</h1> : <h1>{message}</h1>}
                 </LoadingContainer>
             ) : null}
         </>
